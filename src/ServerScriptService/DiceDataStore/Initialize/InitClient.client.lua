4722ddbd52078ada8c4698024c85832d628abec1
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-09-11 14:04:17 -0400

    rewrote datastore; v2 release

--- src/ServerScriptService/DiceDataStore/Initialize/InitClient.client.lua
+++ src/ServerScriptService/DiceDataStore/Initialize/InitClient.client.lua
@@ -8,8 +8,8 @@ end})
 local Player = Services['Players'].LocalPlayer
 if script:IsDescendantOf(Player) then
 	local findModule = Services['ReplicatedStorage']:FindFirstChild(script.DataStoreName.Value,true)
-	local currentClock = tick()
-	while not findModule and tick() - currentClock < 5 do
+	local currentClock = os.clock()
+	while not findModule and os.clock() - currentClock < 5 do
 		for index,modules in pairs(Services['ReplicatedStorage']:GetDescendants()) do
 			if modules.Name == script.DataStoreName.Value then
 				findModule = modules
